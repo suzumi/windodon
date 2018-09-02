c19308bdcb8e6012e601f1a1b55e9dbeccb65bc2
suzumi <ganimata39@gmail.com>
2018-09-02 19:02:27 +0900

    Initial Commit

---
+++ windodon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  windodon
+//
+//  Created by 渋江一晃 on 2018/09/02.
+//  Copyright © 2018年 suzumi49n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
